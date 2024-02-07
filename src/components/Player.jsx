1e1f47a56ecaede3c84f00ffba73d316eeb32dd6
Lorenzo Gaviglio <lorenzotgaviglio97@gmail.com>
2024-02-07 19:37:02 +0100

    Players board

---
+++ src/components/Player.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [playerName, setPlayerName] = useState(name);
+  const [isEditing, setIsEditing] = useState(false);
+  let playerNameHolder = <span className="player-name">{playerName}</span>;
+
+  const playerEditHandler = () => {
+    setIsEditing((prevEditValue) => {
+      return !prevEditValue;
+    });
+  };
+
+  const nameChangeHandler = (event) => {
+    setPlayerName(event.target.value);
+  };
+
+  if (isEditing) {
+    playerNameHolder = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={nameChangeHandler}
+      />
+    );
+  }
+
+  return (
+    <>
+      <li>
+        <span className="player">
+          {playerNameHolder}
+          <span className="player-symbol">{symbol}</span>
+        </span>
+        <button onClick={playerEditHandler}>
+          {isEditing === false ? "Edit" : "Save"}
+        </button>
+      </li>
+    </>
+  );
+}
