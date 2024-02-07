6ac1d07f9fc9ffdd836bb4c8f4ef146a5d2017f2
Lorenzo Gaviglio <lorenzotgaviglio97@gmail.com>
2024-02-07 20:03:28 +0100

    Basic game grid layout

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,8 +6,18 @@ const initialGameBoard = [
 
 export default function GameBoard() {
   return (
-    <>
-      <ol className="game-board"></ol>
-    </>
+    <ol id="game-board">
+      {initialGameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
   );
 }
